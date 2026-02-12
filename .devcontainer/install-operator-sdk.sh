bcfc35a965ca6a128b71fa60525725d70956cf5c
Dan Taylor <dantaylor1996@gmail.com>
2026-02-12 12:26:23 +0000

    Part way through initialization. operator doesn't like some files existing

--- .devcontainer/install-operator-sdk.sh
+++ .devcontainer/install-operator-sdk.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 export ARCH=$(case $(uname -m) in x86_64) echo -n amd64 ;; aarch64) echo -n arm64 ;; *) echo -n $(uname -m) ;; esac)
 export OS=$(uname | awk '{print tolower($0)}')
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dante-lor/multen-operator
+
+go 1.25.0
